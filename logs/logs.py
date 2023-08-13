3396e328b33eec5f5c1548684e61365a4585c469
mmishv <pinina7773@gmail.com>
2023-08-13 15:34:34 +0300

    update settings

--- logs/logs.py
+++ logs/logs.py
@@ -2,8 +2,14 @@ import logging.config
 
 import yaml
 
+from src.settings import Settings
 
-def configure_logger(config_path):
+settings = Settings()
+
+
+def configure_logger(name):
+    config_path = settings.CONFIG_LOGGER_PATH
     with open(config_path, "r") as stream:
         config = yaml.safe_load(stream)
-        logging.config.dictConfig(config)
+    logging.config.dictConfig(config)
+    return logging.getLogger(name)
