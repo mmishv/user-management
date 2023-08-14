07ccb8211401323911189329a6041ceb5a65db87
mmishv <pinina7773@gmail.com>
2023-08-14 11:38:29 +0300

    change the use of enum and 1:m relation for models

--- src/models.py
+++ src/models.py
@@ -1,7 +1,8 @@
+import enum
 import uuid
+from datetime import datetime
 
-import timestamp
-from sqlalchemy import Enum, ForeignKey, Integer, String, types
+from sqlalchemy import DateTime, Enum, ForeignKey, Integer, String, types
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
@@ -9,7 +10,7 @@ class Base(DeclarativeBase):
     pass
 
 
-class RoleEnum(Enum):
+class RoleEnum(str, enum.Enum):
     USER = "USER"
     ADMIN = "ADMIN"
     MODERATOR = "MODERATOR"
@@ -20,15 +21,15 @@ class Group(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(100), index=True)
-    created_at: Mapped[timestamp]
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
-    users: Mapped[list] = relationship(
+    users = relationship(
         "User", back_populates="group", order_by="User.username", lazy="joined"
     )
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id: Mapped[uuid] = mapped_column(
         types.Uuid(), primary_key=True, index=True, default=uuid.uuid4
@@ -38,13 +39,13 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(100), unique=True, index=True)
     phone_number: Mapped[str] = mapped_column(String(20), unique=True)
     email: Mapped[str] = mapped_column(String(256), unique=True, index=True)
-    role: Mapped[RoleEnum] = mapped_column(RoleEnum, default=RoleEnum.USER)
-    group_id: Mapped[int] = mapped_column(ForeignKey("groups.id"))
+    role: Mapped[RoleEnum] = mapped_column(Enum(RoleEnum), default=RoleEnum.USER)
+    group_id: Mapped[int] = mapped_column(Integer, ForeignKey("group.id"))
     image_s3_path: Mapped[str] = mapped_column(String(1024))
     is_blocked: Mapped[bool] = mapped_column(default=False)
-    created_at: Mapped[timestamp]
-    modified_at: Mapped[timestamp]
+    created_at: Mapped[datetime]
+    modified_at: Mapped[datetime]
 
-    group: Mapped[list] = relationship(
+    group = relationship(
         "Group", back_populates="users", order_by="Group.id", lazy="joined"
     )
