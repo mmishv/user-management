fb564b0ec84f203decb2bf3eac2ab46af3367912
mmishv <pinina7773@gmail.com>
2023-08-17 10:58:58 +0300

    update models

--- src/models.py
+++ src/models.py
@@ -2,7 +2,7 @@ import enum
 import uuid
 from datetime import datetime
 
-from sqlalchemy import DateTime, Enum, ForeignKey, Integer, String, types
+from sqlalchemy import DateTime, Enum, ForeignKey, Integer, Text, types
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
@@ -20,7 +20,7 @@ class Group(Base):
     __tablename__ = "group"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String(100), index=True)
+    name: Mapped[str] = mapped_column(Text, index=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
     users = relationship(
@@ -34,18 +34,20 @@ class User(Base):
     id: Mapped[uuid] = mapped_column(
         types.Uuid(), primary_key=True, index=True, default=uuid.uuid4
     )
-    name: Mapped[str]
-    surname: Mapped[str]
-    username: Mapped[str] = mapped_column(String(100), unique=True, index=True)
-    hashed_password: Mapped[str] = mapped_column(String(255))
-    phone_number: Mapped[str] = mapped_column(String(20), unique=True)
-    email: Mapped[str] = mapped_column(String(256), unique=True, index=True)
+    name: Mapped[str] = mapped_column(Text, nullable=True)
+    surname: Mapped[str] = mapped_column(Text, nullable=True)
+    username: Mapped[str] = mapped_column(Text, unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(Text)
+    phone_number: Mapped[str] = mapped_column(Text, unique=True, nullable=True)
+    email: Mapped[str] = mapped_column(Text, unique=True, index=True)
     role: Mapped[RoleEnum] = mapped_column(Enum(RoleEnum), default=RoleEnum.USER)
-    group_id: Mapped[int] = mapped_column(Integer, ForeignKey("group.id"))
-    image_s3_path: Mapped[str] = mapped_column(String(1024))
+    group_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("group.id"), nullable=True
+    )
+    image_s3_path: Mapped[str] = mapped_column(Text, nullable=True)
     is_blocked: Mapped[bool] = mapped_column(default=False)
-    created_at: Mapped[datetime]
-    modified_at: Mapped[datetime]
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    modified_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
 
     group = relationship(
         "Group", back_populates="users", order_by="Group.id", lazy="joined"
