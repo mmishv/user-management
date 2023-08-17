9d9a434436d352fb48f7b0609ce52e60ffeb2750
mmishv <pinina7773@gmail.com>
2023-08-17 11:04:26 +0300

    test db connection

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import asyncio
+from typing import Generator
+
+import pytest
+
+from src.database import engine
+from src.models import Base
+
+
+@pytest.fixture(scope="session")
+def event_loop(request) -> Generator:
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="function")
+async def truncate_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    await engine.dispose()
