197a3cd5aaf6bea52cbd5cf021d32c5df73ab8a3
mmishv <pinina7773@gmail.com>
2023-08-18 14:53:15 +0300

    test users routes

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,8 +1,13 @@
+from datetime import datetime
+
 import pytest
 from httpx import AsyncClient
 
 from src.auth.schemas import UserLogin, UserSignup
+from src.database import create_async_session
 from src.main import app
+from src.models import Group, User
+from src.utils import get_current_user
 
 client_base_url = "http://test"
 
@@ -59,3 +64,69 @@ async def reset_password_token(user_signup_data, create_user):
     link = response.json().get("reset_link")
     token = link.split("=")[-1]
     return token
+
+
+@pytest.fixture
+@pytest.mark.asyncio
+async def create_user_and_admin(create_user):
+    user_access_token = (await create_user)[0]
+    async with AsyncClient(app=app, base_url=client_base_url) as client:
+        response = await client.post(
+            SIGNUP_URL,
+            json=UserSignup(
+                username="admin", email="admin@gmail.com", password="1111"
+            ).model_dump(),
+        )
+    admin_access_token = response.json().get("access_token")
+    admin = await get_current_user(admin_access_token)
+    async with create_async_session() as conn:
+        admin = await conn.get(User, admin.id)
+        admin.role = "ADMIN"
+        await conn.commit()
+    return admin_access_token, user_access_token
+
+
+@pytest.fixture
+@pytest.mark.asyncio
+async def create_user_and_moderator_from_the_same_group(create_user):
+    user_access_token = (await create_user)[0]
+    async with AsyncClient(app=app, base_url=client_base_url) as client:
+        response = await client.post(
+            SIGNUP_URL,
+            json=UserSignup(
+                username="moderator", email="moderator@gmail.com", password="1111"
+            ).model_dump(),
+        )
+    access_token = response.json().get("access_token")
+    user = await get_current_user(user_access_token)
+    moderator = await get_current_user(access_token)
+    async with create_async_session() as conn:
+        group = Group(name="first_group", created_at=datetime.utcnow())
+        conn.add(group)
+        await conn.commit()
+        user = await conn.get(User, user.id)
+        user.group_id = group.id
+        moderator = await conn.get(User, moderator.id)
+        moderator.group_id = group.id
+        await conn.commit()
+    return access_token, user_access_token
+
+
+@pytest.fixture
+@pytest.mark.asyncio
+async def create_user_and_moderator_from_diff_group(
+    create_user_and_moderator_from_the_same_group,
+):
+    (
+        moderator_access_token,
+        user_access_token,
+    ) = await create_user_and_moderator_from_the_same_group
+    moderator = await get_current_user(moderator_access_token)
+    async with create_async_session() as conn:
+        group = Group(name="second_group", created_at=datetime.utcnow())
+        conn.add(group)
+        await conn.commit()
+        moderator = await conn.get(User, moderator.id)
+        moderator.group_id = group.id
+        await conn.commit()
+    return moderator_access_token, user_access_token
--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -2,15 +2,201 @@ import pytest
 from httpx import AsyncClient
 
 from src.main import app
-from tests.fixtures import client_base_url, create_user, user_signup_data
+from src.users.schemas import UserData
+from src.utils import get_current_user
+from tests.fixtures import (
+    client_base_url,
+    create_user,
+    create_user_and_admin,
+    create_user_and_moderator_from_diff_group,
+    create_user_and_moderator_from_the_same_group,
+    user_signup_data,
+)
 
-USERS_ME_URL = "/users/me/"
+USERS_URL = "/users"
+
+
+@pytest.mark.parametrize(
+    "tokens_fixture",
+    ["create_user_and_admin", "create_user_and_moderator_from_the_same_group"],
+)
+@pytest.mark.asyncio
+async def test_successful_get_user(tokens_fixture, truncate_tables, request):
+    await truncate_tables
+    admin_access_token, user_access_token = await request.getfixturevalue(
+        tokens_fixture
+    )
+    user = await get_current_user(user_access_token)
+    async with AsyncClient(app=app, base_url=client_base_url) as client:
+        response = await client.get(
+            f"{USERS_URL}/{user.id}/", headers={"token": admin_access_token}
+        )
+    assert response.status_code == 200
+
+    response_data = response.json()
+    assert (
+        UserData(**user.to_dict()).model_dump()
+        == UserData(**response_data).model_dump()
+    )
+
+
+@pytest.mark.parametrize(
+    "tokens_fixture", ["create_user", "create_user_and_moderator_from_diff_group"]
+)
+@pytest.mark.asyncio
+async def test_no_permissions_get_user(tokens_fixture, truncate_tables, request):
+    await truncate_tables
+    admin_access_token, user_access_token = await request.getfixturevalue(
+        tokens_fixture
+    )
+    user = await get_current_user(user_access_token)
+    async with AsyncClient(app=app, base_url=client_base_url) as client:
+        response = await client.get(
+            f"{USERS_URL}/{user.id}/", headers={"token": admin_access_token}
+        )
+    assert response.status_code == 403
 
 
 @pytest.mark.asyncio
-async def test_successful_get_me(truncate_tables, create_user):
+async def test_successful_patch_user(truncate_tables, create_user_and_admin):
     await truncate_tables
-    access_token = (await create_user)[0]
+    admin_access_token, user_access_token = await create_user_and_admin
+    user = await get_current_user(user_access_token)
     async with AsyncClient(app=app, base_url=client_base_url) as client:
-        response = await client.get(USERS_ME_URL, headers={"token": access_token})
+        response = await client.patch(
+            f"{USERS_URL}/{user.id}/", headers={"token": admin_access_token}, json={}
+        )
+
     assert response.status_code == 200
+
+    response_data = response.json()
+    user.modified_at = response_data.get("modified_at")
+    assert (
+        UserData(**user.to_dict()).model_dump()
+        == UserData(**response_data).model_dump()
+    )
+
+
+@pytest.mark.parametrize(
+    "tokens_fixture",
+    [
+        "create_user",
+        "create_user_and_moderator_from_the_same_group",
+        "create_user_and_moderator_from_diff_group",
+    ],
+)
+@pytest.mark.asyncio
+async def test_no_permissions_patch_user(tokens_fixture, truncate_tables, request):
+    await truncate_tables
+    admin_access_token, user_access_token = await request.getfixturevalue(
+        tokens_fixture
+    )
+    user = await get_current_user(user_access_token)
+    async with AsyncClient(app=app, base_url=client_base_url) as client:
+        response = await client.patch(
+            f"{USERS_URL}/{user.id}/", headers={"token": admin_access_token}, json={}
+        )
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_successful_delete_user(truncate_tables, create_user_and_admin):
+    await truncate_tables
+    admin_access_token, user_access_token = await create_user_and_admin
+    user = await get_current_user(user_access_token)
+    async with AsyncClient(app=app, base_url=client_base_url) as client:
+        response = await client.delete(
+            f"{USERS_URL}/{user.id}/", headers={"token": admin_access_token}
+        )
+
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "tokens_fixture",
+    [
+        "create_user",
+        "create_user_and_moderator_from_the_same_group",
+        "create_user_and_moderator_from_diff_group",
+    ],
+)
+@pytest.mark.asyncio
+async def test_no_permissions_delete_user(tokens_fixture, truncate_tables, request):
+    await truncate_tables
+    access_token, user_access_token = await request.getfixturevalue(tokens_fixture)
+    user = await get_current_user(user_access_token)
+    async with AsyncClient(app=app, base_url=client_base_url) as client:
+        response = await client.delete(
+            f"{USERS_URL}/{user.id}/", headers={"token": access_token}
+        )
+    assert response.status_code == 403
+
+
+@pytest.mark.parametrize(
+    "query_params, expected_status",
+    [
+        ("?page=5&limit=30&sort_by=name&order_by=desc", 200),
+        ("?page=1&limit=30&sort_by=id&order_by=asc", 200),
+        ("?page=1&limit=30&sort_by=id&order_by=desc", 200),
+        ("?page=-1&limit=30", 422),
+        ("?page=1&limit=150", 422),
+        ("?page=1&limit=30&sort_by=invalid_field", 403),
+        ("?page=1&limit=30&order_by=invalid_order", 403),
+    ],
+)
+@pytest.mark.asyncio
+async def test_successful_get_users(
+    truncate_tables, query_params, expected_status, create_user_and_admin
+):
+    await truncate_tables
+    admin_access_token = (await create_user_and_admin)[0]
+    async with AsyncClient(app=app, base_url=client_base_url) as client:
+        response = await client.get(
+            f"{USERS_URL}/{query_params}/", headers={"token": admin_access_token}
+        )
+    assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    "query_params, expected_status",
+    [
+        ("?page=5&limit=30&sort_by=name&order_by=desc", 200),
+        ("?page=1&limit=30&sort_by=id&order_by=asc", 200),
+        ("?page=1&limit=30&sort_by=id&order_by=desc", 200),
+        ("?page=-1&limit=30", 422),
+        ("?page=1&limit=150", 422),
+        ("?page=1&limit=30&sort_by=invalid_field", 403),
+        ("?page=1&limit=30&order_by=invalid_order", 403),
+    ],
+)
+@pytest.mark.asyncio
+async def test_successful_get_users(
+    truncate_tables, query_params, expected_status, create_user_and_admin
+):
+    await truncate_tables
+    admin_access_token = (await create_user_and_admin)[0]
+    async with AsyncClient(app=app, base_url=client_base_url) as client:
+        response = await client.get(
+            f"{USERS_URL}/{query_params}/", headers={"token": admin_access_token}
+        )
+    assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    "tokens_fixture",
+    [
+        "create_user",
+        "create_user_and_moderator_from_the_same_group",
+        "create_user_and_moderator_from_diff_group",
+    ],
+)
+@pytest.mark.asyncio
+async def test_no_permissions_get_users(tokens_fixture, truncate_tables, request):
+    await truncate_tables
+    access_token = (await request.getfixturevalue(tokens_fixture))[0]
+    async with AsyncClient(app=app, base_url=client_base_url) as client:
+        response = await client.get(
+            f"{USERS_URL}/?page=5&limit=30&sort_by=name&order_by=desc/",
+            headers={"token": access_token},
+        )
+    assert response.status_code == 403
